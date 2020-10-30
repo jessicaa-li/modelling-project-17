93906ae54458c39c142cc95c39e9db5b2f312228
Isabella Enriquez <isabellaenriquez@rogers.com>
2020-10-30 02:46:34 +0000

    Declared variables + created format for type strengths and weaknesses

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 from nnf import Var
 from lib204 import Encoding
 
+"""
 # Call your variables whatever you want
 a = Var('a')
 b = Var('b')
@@ -9,9 +10,13 @@ c = Var('c')
 x = Var('x')
 y = Var('y')
 z = Var('z')
+"""
+num_pokemon = 3 # can change this to whatever
+foe = []
+player = []
+types = ["fire", "water", "grass", "electric", "ground", "rock", "ice", "flying"] #idk if we actually need this
 
 
-#
 # Build an example full theory for your setting and return it.
 #
 #  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
@@ -19,9 +24,9 @@ z = Var('z')
 #  what the expectations are.
 def example_theory():
     E = Encoding()
-    E.add_constraint(a | b)
-    E.add_constraint(~a | ~x)
-    E.add_constraint(c | y | z)
+    for i in range(num_pokemon):
+      # if foe is this type >> these types are strong and ~these types are weak against
+      E.add_constraint(foe[i]["fire"] >> ((player[i]["water"] | player[i]["ground"] | player[i]["rock"]) & (~player[i]["grass"] & ~player[i]["ice"]))) # format; do this for the rest of the types
     return E
 
 
